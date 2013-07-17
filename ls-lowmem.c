2e5ebc70c4e8111aa2ac3237dffc3728d0e8aeee
Scott Duckworth <sduckwo@clemson.edu>
2013-07-17 11:29:30 -0400

    clean up ls-lowmem

--- ls-lowmem.c
+++ ls-lowmem.c
@@ -7,10 +7,8 @@
 #include <sys/stat.h>
 #include <sys/syscall.h>
 
-#define handle_error(msg) \
-  do { perror(msg); exit(EXIT_FAILURE); } while (0)
-
-#define BUF_SIZE 32768
+#define ERROR_SIZE 4096
+#define GETDENTS_BUF_SIZE 32768
 
 struct linux_dirent {
   unsigned long  d_ino;
@@ -19,35 +17,62 @@ struct linux_dirent {
   char           d_name[];
 };
 
-int main(int argc, char *argv[]) {
-  int fd, nread;
-  char buf[BUF_SIZE];
+static char error_message[ERROR_SIZE];
+
+static int ls_lowmem(const char *dirname) {
+  int fd, nread, bpos, rc=0;
+  char buf[GETDENTS_BUF_SIZE];
   struct linux_dirent *d;
-  int bpos, i;
-  const char *dirname;
 
-  for(i = 1; i < argc; ++i) {
-    dirname = argv[i];
-    fd = open(dirname, O_RDONLY | O_DIRECTORY);
-    if(fd == -1)
-      handle_error(dirname);
-
-    for(;;) {
-      nread = syscall(SYS_getdents, fd, buf, BUF_SIZE);
-      if(nread == -1)
-        handle_error(dirname);
-      if(nread == 0)
-        break;
-
-      for(bpos = 0; bpos < nread; bpos += d->d_reclen) {
-        d = (struct linux_dirent *)(buf + bpos);
-        printf("%s\n", d->d_name);
-      }
-    }
-
-    if(close(fd) == -1)
-      handle_error(dirname);
+  fd = open(dirname, O_RDONLY | O_DIRECTORY);
+  if(fd == -1) {
+    perror(dirname);
+    return 1;
   }
 
-  exit(EXIT_SUCCESS);
+  for(;;) {
+    nread = syscall(SYS_getdents, fd, buf, sizeof(buf));
+    if(nread == -1) {
+      snprintf(error_message, sizeof(error_message), "getdents failed on %s", dirname);
+      rc = 1;
+      goto out;
+    }
+    if(nread == 0)
+      break;
+
+    for(bpos = 0; bpos < nread; bpos += d->d_reclen) {
+      d = (struct linux_dirent *)(buf + bpos);
+      printf("%s\n", d->d_name);
+    }
+  }
+
+out:
+  if(close(fd) == -1) {
+    perror(dirname);
+    rc = 1;
+  }
+
+  return rc;
+}
+
+int main(int argc, char *argv[]) {
+  int i, fail=0;
+  const char *dirname;
+
+  if(argc == 1) {
+    ls_lowmem(".");
+  } else {
+    for(i = 1; i < argc; ++i) {
+      dirname = argv[i];
+      if(argc > 2) {
+        printf("%s:\n", dirname);
+        ls_lowmem(dirname);
+        printf("\n");
+      } else {
+        fail |= ls_lowmem(dirname);
+      }
+    }
+  }
+
+  exit(fail ? EXIT_FAILURE : EXIT_SUCCESS);
 }
